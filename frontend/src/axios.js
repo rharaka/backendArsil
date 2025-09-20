45fd7825755ee83545e31cdd10f41c5d5cc934a1
WebRachDotCom <haraka.rachid@gmail.com>
2025-09-20 17:54:02 +0200

    Update axios.js
    Basic URL remote

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://127.0.0.1:5000/api'
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://arsil-backend-anzxy.sevalla.app/api'
 });
 
 export default instance;
